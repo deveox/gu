805ba6e8fcfe168b9e0a9ffa9953ae7520974ea8
David Iunevich <f3oall@gmail.com>
2024-04-06 20:42:38 -0400

    feat: added new to the mirror

--- mirror/mirror.go
+++ mirror/mirror.go
@@ -1,6 +1,8 @@
 package mirror
 
-import "reflect"
+import (
+	"reflect"
+)
 
 func DerefType(t reflect.Type) reflect.Type {
 	for {
@@ -21,5 +23,21 @@ func DerefValue(v reflect.Value) reflect.Value {
 		default:
 			return v
 		}
+	}
+}
+
+// New creates a new instance of the given generic type.
+//
+// v := New[int]() // v is int
+// v := New[*int]() // v is *int
+// v := New[**int]() is not supported
+func New[T any]() (T, reflect.Type) {
+	t := reflect.TypeFor[T]()
+	i := reflect.New(DerefType(t))
+	switch t.Kind() {
+	case reflect.Ptr, reflect.Interface:
+		return i.Interface().(T), t
+	default:
+		return i.Elem().Interface().(T), t
 	}
 }
--- mirror/mirror_test.go
+++ mirror/mirror_test.go
@@ -4,18 +4,32 @@ import (
 	"reflect"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
 func TestDerefType(t *testing.T) {
 	typ := reflect.TypeOf(&struct{}{})
-	require.Equal(t, reflect.TypeOf(struct{}{}), DerefType(typ))
+	require.Equal(t, reflect.TypeOf(struct{}{}).Name(), DerefType(typ).Name())
 	v := 0
 	v1 := &v
 	v2 := &v1
 	typ = reflect.TypeOf(v2)
-	require.Equal(t, reflect.TypeOf(0), DerefType(typ))
+	require.Equal(t, reflect.TypeOf(v).Name(), DerefType(typ).Name())
 	var iv interface{} = 0
 	typ = reflect.TypeOf(&iv)
-	require.Equal(t, reflect.TypeOf(0), DerefType(typ))
+	require.Equal(t, reflect.TypeFor[interface{}]().Name(), DerefType(typ).Name())
+}
+
+func TestNew(t *testing.T) {
+	type MyStruct struct{}
+
+	actual, typ := New[MyStruct]()
+	assert.Equal(t, reflect.TypeOf(MyStruct{}).Name(), typ.Name(), "New() should return the type of the given type")
+	assert.NotNil(t, actual, "New() should not return nil when called with a non-pointer type")
+	assert.Equal(t, actual, MyStruct{}, "New() should return a zero value of the given type")
+	actual2, typ := New[*MyStruct]()
+	assert.Equal(t, reflect.TypeOf(&MyStruct{}).Name(), typ.Name(), "New() should return the type of the given type")
+	assert.NotNil(t, actual2, "New() should not return nil when called with a pointer type")
+	assert.Equal(t, actual2, &MyStruct{}, "New() should return a zero value of the given type")
 }
